5cd32c42644677baa64fcb1c98b1f213972ad694
Kenneth Stanley <ken@stanleysoft.org>
2018-07-21 16:09:11 -0400

    Fix some visual issues

--- views/admin/admin-nav.php
+++ views/admin/admin-nav.php
@@ -1,18 +1,18 @@
 <form method="post">
     <div class="btn-toolbar" id="btns-report-group" role="group" aria-label="Admin Nav">
-        <button type="submit" id="btn-avg" name="btn-avg" class="btn btn-info">Average Rate</button>
-        <button type="submit" id="btn-norent" name="btn-norent" class="btn btn-info">No Rent</button>
-        <button type="submit" class="btn btn-info">view3</button>
-        <button type="submit" class="btn btn-info">view4</button>
-        <button type="submit" class="btn btn-info">view5</button>
-        <button type="submit" class="btn btn-info">view6</button>
-        <button type="submit" class="btn btn-info">view7</button>
-        <button type="submit" class="btn btn-info">view8</button>
-        <button type="submit" class="btn btn-info">view9</button>
+        <button type="submit" id="btn-avg" name="btn-avg" class="btn btn-info btn-sm">Average Rate</button>
+        <button type="submit" id="btn-norent" name="btn-norent" class="btn btn-info btn-sm">Never Rented</button>
+        <button type="submit" class="btn btn-info btn-sm">Freq Renters</button>
+        <button type="submit" class="btn btn-info btn-sm">2014 Renters</button>
+        <button type="submit" class="btn btn-info btn-sm">OV Rate Inc</button>
+        <button type="submit" class="btn btn-info btn-sm">Earning</button>
+        <button type="submit" class="btn btn-info btn-sm">Pmt Types</button>
+        <button type="submit" class="btn btn-info btn-sm">Renter Pet</button>
+        <button type="submit" class="btn btn-info btn-sm">Times Rented</button>
         <?php 
             if($rank === 'super_admin'){
                 ?>
-                <button type="submit" id="reset-db" class="btn btn-danger">RESET DATABASE</button>
+                <button type="submit" id="reset-db" class="btn btn-danger btn-sm">RESET DATABASE</button>
                 <?php
             }
         ?>
